3edd4344735e6ee8a90be2ad984898b539a6f6cd
firayakhadis <112853822+firayakhadis@users.noreply.github.com>
2022-10-11 13:21:39 +0300

    changes

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -4,6 +4,11 @@
             <div class="column is-12">
                 <h2 class="is-size-2 has-text-centered">{{ category.name }}</h2>
             </div>
+
+            <ProductBox
+                v-for="product in category.products"
+                v-bind:key="product.id"
+                v-bind:product="product" />
         </div>
     </div>
 </template>
@@ -12,8 +17,13 @@
 import axios from 'axios'
 import {toast} from 'bulma-toast'
 
+import ProductBox from '@/components/ProductBox'
+
 export default {
     name: 'Category',
+    components: {
+        ProductBox
+    },
     data() {
         return {
             category: {
@@ -24,11 +34,18 @@ export default {
     mounted() {
         this.getCategory()
     },
+    watch: {
+        $route(to, from) {
+            if (to.name === 'Category') {
+                this.getCategory()
+            }
+        }
+    },
     methods: {
         async getCategory() {
             const categorySlug = this.$route.params.category_slug
 
-            this.$route.commit('setIsLoading', true)
+            this.$store.commit('setIsLoading', true)
 
             axios
                 .get(`/api/v1/products/${categorySlug}/`)
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,28 +16,18 @@
           <h2 class="is-size-2 has-text-centered">Latest products</h2>
       </div>
 
-      <div
-        class="column is-3"
+      <ProductBox
         v-for="product in latestProducts"
         v-bind:key="product.id"
-      >
-        <div class="box">
-          <figure class="image mb-4">
-            <img :src="product.get_thumbnail">
-          </figure>
-
-          <h3 class="is-size-4">{{ product.name }}</h3>
-          <p class="is-size-6 has-text-grey">${{ product.name }}</p>
-
-          <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View details</router-link>
-        </div>
-      </div>
+        v-bind:product="product" />
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+
+import ProductBox from '@/components/ProductBox'
 
 export default {
   name: 'Home',
@@ -47,6 +37,7 @@ export default {
     }
   },
   components: {
+    ProductBox
   },
   mounted() {
     this.getLatestProducts()
@@ -74,10 +65,3 @@ export default {
 }
 </script>
 
-<style scoped>
-  .image {
-    margin-top: -1.25rem;
-    margin-left: -1.25rem;
-    margin-right: -1.25rem;
-  }
-</style>
