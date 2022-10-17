f7dd7ecc22da42aa7e2c7b51e3fd36a62dc6a0c9
firayakhadis <112853822+firayakhadis@users.noreply.github.com>
2022-10-17 18:51:48 +0300

    changes

--- src/views/MyAccount.vue
+++ src/views/MyAccount.vue
@@ -1,12 +1,23 @@
 <template>
     <div class="page-my-account">
         <div class="columns is-multiline">
-            <div class="columns is-12">
+            <div class="column is-12">
                 <h1 class="title">My account</h1>
             </div>
 
             <div class="column is-12">
                 <button @click="logout()" class="button is-danger">Log out</button>
+            </div>
+
+            <hr>
+
+            <div class="column is-12">
+                <h2 class="subtitle">My orders</h2>
+
+                <OrderSummary
+                    v-for="order in orders"
+                    v-bind:key="order.id"
+                    v-bind:order="order" />
             </div>
         </div>
     </div>
@@ -15,8 +26,23 @@
 <script>
 import axios from 'axios'
 
+import OrderSummary from '@/components/OrderSummary.vue'
+
 export default {
     name: 'MyAccount',
+    components: {
+        OrderSummary
+    },
+    data() {
+        return {
+            orders: []
+        }
+    },
+    mounted() {
+        document.title = 'My account | Store'
+
+        this.getMyOrders()
+    },
     methods: {
         logout() {
             axios.defaults.headers.common["Authorization"] = ""
@@ -28,7 +54,21 @@ export default {
             this.$store.commit('removeToken')
 
             this.$router.push('/')
-       }
+        },
+        async getMyOrders() {
+            this.$store.commit('setIsLoading', true)
+
+            await axios
+                .get('/api/v1/orders/')
+                .then(response => {
+                    this.orders = response.data
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+
+            this.$store.commit('setIsLoading', false)
+        }
     }
 }
 </script>
